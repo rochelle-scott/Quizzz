35cbb266e54cd298560ebf999f2a0a324c53e274
rochelle-scott <rochellescott.dev@gmail.com>
2020-10-08 15:32:59 +0100

    Added: Show quiz questions

--- Quizzz/ViewController.swift
+++ Quizzz/ViewController.swift
@@ -14,15 +14,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    //Want more questions so need to add array.
+    let quiz = [
+        "How many times has Arsenal won the FA Cup?",
+        "How many teams compete within the Premier League?",
+        "Which Premier League team is also known by the nickname 'Toffees'?"
+     ]
+    
+    //variable to keep track of which q the user is currently reading
+    var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1 //questionNumber = questionNumber + 1
+        updateUI()
     }
     
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
+    }
     
 }
 
