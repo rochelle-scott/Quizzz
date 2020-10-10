672681642fddec87729fc847e8bd53f991566dcc
rochelle-scott <rochellescott.dev@gmail.com>
2020-10-10 19:04:24 +0100

    Added progress bar.

--- Quizzz/ViewController.swift
+++ Quizzz/ViewController.swift
@@ -21,8 +21,19 @@ class ViewController: UIViewController {
     let quiz = [
         Question(q: "Arsenal won the FA Cup 14 times.", a: "True"),
         Question(q: "40 teams compete within the Premier League.", a: "False"),
-        Question(q: "Premier League team Burnley is also known by the nickname 'Toffees'.", a: "False")
-     ]
+        Question(q: "Premier League team Burnley is also known by the nickname 'Toffees'.", a: "False"),
+        Question(q: "A slug's blood is green.", a: "True"),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")     ]
 
     
     //variable to keep track of which question the user is currently reading.
@@ -42,9 +53,9 @@ class ViewController: UIViewController {
         
         //Check to see if there chosen answer is correct.
         if userAnswer == actualAnswer {
-            print("Correct!")
+            sender.backgroundColor = UIColor.green
         } else {
-            print("Incorrect!")
+            sender.backgroundColor = UIColor.red
         }
         
         
@@ -53,12 +64,18 @@ class ViewController: UIViewController {
         } else {
             questionNumber = 0 // this will fetch first question again. It allows to loop.
         }
-          updateUI()
+        
+       Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
         
     }
     
-    func updateUI() {
+    @objc func updateUI() {
         questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
+        
     }
     
 }
